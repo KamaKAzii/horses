860df9d90edef5316aabf7ce316b78ac28961b55
James Kozianski <koz@chromium.org>
2013-10-19 13:19:31 +1100

    Horses and bets.

--- server.go
+++ server.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 type Player struct {
@@ -12,8 +13,21 @@ type Player struct {
 	Money int
 }
 
+type Horse struct {
+	Id   string
+	Name string
+}
+
+type Bet struct {
+	PlayerId string
+	HorseId  string
+	Amount   int
+}
+
 type World struct {
 	Players map[string]Player
+	Horses  map[string]Horse
+	Bets    []Bet
 }
 
 func (world *World) ServeHTTP(w http.ResponseWriter, req *http.Request) {
@@ -21,11 +35,26 @@ func (world *World) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	mux.Handle("/static/", http.StripPrefix("/static/",
 		http.FileServer(http.Dir("static"))))
 	mux.HandleFunc("/api/getPlayers", world.getPlayers)
+	mux.HandleFunc("/api/placeBet", world.placeBet)
 	mux.ServeHTTP(w, req)
 }
 
 func (world *World) getPlayers(w http.ResponseWriter, req *http.Request) {
 	ReplyWithJson(w, req, world)
+}
+
+func (world *World) placeBet(w http.ResponseWriter, req *http.Request) {
+	req.ParseForm()
+	log.Print(req.Form)
+	player_id := req.Form["Player"][0]
+	horse_id := req.Form["Horse"][0]
+	amount, err := strconv.Atoi(req.Form["Amount"][0])
+	if err != nil {
+		http.Error(w, "fail", 500)
+		return
+	}
+	world.Bets = append(world.Bets, Bet{player_id, horse_id, amount})
+	ReplyWithJson(w, req, "ok")
 }
 
 func ReplyWithJson(w http.ResponseWriter, req *http.Request, i interface{}) {
@@ -43,13 +72,13 @@ func NewWorld() (world *World) {
 	players["1"] = Player{"1", "James", 100}
 	players["2"] = Player{"2", "Emmet", 100}
 	players["3"] = Player{"3", "Michelle", 100}
-	return &World{players}
+	return &World{players, make(map[string]Horse), make([]Bet, 0)}
 }
 
 func main() {
 	world := NewWorld()
 	s := &http.Server{
-		Addr: ":8080",
+		Addr:    ":8080",
 		Handler: world,
 	}
 	log.Fatal(s.ListenAndServe())
